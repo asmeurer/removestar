16b69037628bbba34abc78cd246e053fba4c340f
Aaron Meurer <asmeurer@gmail.com>
2019-07-18 16:43:41 -0600

    Add some limitations

--- removestar.py
+++ removestar.py
@@ -3,6 +3,12 @@
 Tool to automatically replace "import *" imports with explicit imports
 
 Requires pyflakes. Somewhat inspired by autoflake.
+
+Limitations:
+
+- Only works with relative imports at the moment
+- Does not work correctly with recursive star imports
+- Assumes only names in the current file are used by star imports
 """
 
 from pyflakes.checker import Checker, _MAGIC_GLOBALS, ModuleScope
