98c8b957e1647f13168541f3907fb1da7a5d5993
Aaron Meurer <asmeurer@gmail.com>
2019-07-28 10:46:14 -0600

    Typo

--- removestar/removestar.py
+++ removestar/removestar.py
@@ -115,7 +115,7 @@ def get_names_from_dir(mod, directory):
         else:
             filename = os.path.join(loc, '__init__.py')
         if not os.path.isfile(filename):
-            raise RuntimeError(f"Could not fine the file for the module {mod}")
+            raise RuntimeError(f"Could not find the file for the module {mod}")
     else:
         top, *rest = mod.split('.')
 
