ec0a459df7523dd85bd8679dea46059416cf0d06
Aaron Meurer <asmeurer@gmail.com>
2019-08-05 13:21:07 -0600

    Clean up the description in --help

--- removestar/__main__.py
+++ removestar/__main__.py
@@ -2,13 +2,7 @@
 """
 Tool to automatically replace "import *" imports with explicit imports
 
-Requires pyflakes. Somewhat inspired by autoflake.
-
-Limitations:
-
-- Only works with relative imports at the moment
-- Does not work correctly with recursive star imports
-- Assumes only names in the current file are used by star imports
+Requires pyflakes.
 
 Usage:
 
