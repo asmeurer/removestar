c8fe4b11a04f7df829424618297e9a65f431070b
Aaron Meurer <asmeurer@gmail.com>
2019-08-17 11:44:47 -0600

    Fix the module example in the __main__ docstring

--- removestar/__main__.py
+++ removestar/__main__.py
@@ -10,7 +10,7 @@ $ removestar file.py # Shows diff but does not edit file.py
 
 $ removestar -i file.py # Edits file.py in-place
 
-$ removestar module/ # Modifies every Python file in module recursively
+$ removestar -i module/ # Modifies every Python file in module/ recursively
 
 """
 from . import __version__
